7623780688efc7258ee74680d5fcf8db86a5ab5c
robigan <sebastian.colombo06@gmail.com>
2021-03-22 12:56:55 +0100

    Yes

--- src/Client.js
+++ src/Client.js
@@ -1,13 +1,13 @@
 const SnowTransfer = require("snowtransfer");
 
-const FileSystem = require("fs/promises");
-
 module.exports = class Backer {
     constructor(Token) {
         const Rest = new SnowTransfer(Token);
         this.Rest = Rest;
 
         this.UI = new (require("./UI.js"))(this);
+        this.Snapshot = new (require("./Snapshot.js"))(this);
+        this.Restore = new (require("./Restorer.js"))(this);
         this.Intents = ["General", "Channels", "Roles"];
 
         this.guilds = new Map();
@@ -21,35 +21,8 @@ module.exports = class Backer {
         })().catch(console.error);
     }
 
-    async createSnapshot(guildID) {
-        const guild = this.guilds.get(guildID);
-        const data = {};
-        this.Intents.find(element => element === "General") ? data.general = await this.createGuildGeneral(guildID) : {};
-        this.Intents.find(element => element === "Channels") ? data.channels = guild.channels.cache : {};
-        this.Intents.find(element => element === "Roles") ? data.roles = guild.roles.cache : {};
-        this.Intents.find(element => element === "Emojis") ? data.emojis = guild.emojis.cache : {};
-        this.Intents.find(element => element === "Members") ? data.members = guild.members.cache : {};
-        const today = new Date();
-
-        await FileSystem.appendFile(`${process.cwd()}/Snapshots/${guild.name}-${today.getTime()}.json`, JSON.stringify(data), { flag: "ax" }).then(() => console.log("Snapshot successfully created")).catch((err) => console.error(err));
-
-        this.UI.mainMenu();
-    }
-    // ${guildID}/${today.getUTCFullYear()}/${today.getUTCMonth()}/${today.getUTCDate()}/
-
-    async createGuildGeneral(guildID) {
-        const guild = this.guilds.cache.get(guildID);
-        return {
-            name: guild.name,
-            acronym: guild.nameAcronym,
-            id: guild.id,
-            owner: guild.owner.user.username,
-            ownerID: guild.ownerID
-        };
-    }
-
     async escalateGuild(guildID) {
-        return Object.assign(await this.Rest.guild.getGuild(guildID), 
+        return Object.assign(await this.Rest.guild.getGuild(guildID),
             {channels: await this.Rest.guild.getGuildChannels(guildID)},
         );
     }
--- src/Snapshot.js
+++ src/Snapshot.js
@@ -1,5 +1,33 @@
-module.exports = class Snapshot {
+const FileSystem = require("fs/promises");
+
+module.exports = class Snapshoter {
     constructor(client) {
         this.client = client;
+    }
+
+    async createSnapshot(guildID) {
+        const guild = this.client.guilds.get(guildID);
+        const data = {};
+        this.Intents.find(element => element === "General") ? data.general = await this.createGuildGeneral(guildID) : {};
+        this.Intents.find(element => element === "Channels") ? data.channels = guild.channels.cache : {};
+        this.Intents.find(element => element === "Roles") ? data.roles = guild.roles.cache : {};
+        this.Intents.find(element => element === "Emojis") ? data.emojis = guild.emojis.cache : {};
+        this.Intents.find(element => element === "Members") ? data.members = guild.members.cache : {};
+        const today = new Date();
+
+        await FileSystem.appendFile(`${process.cwd()}/Snapshots/${guild.name}-${today.getTime()}.json`, JSON.stringify(data), { flag: "ax" }).then(() => console.log("Snapshot successfully created")).catch((err) => console.error(err));
+
+        this.UI.mainMenu();
+    }
+    // ${guildID}/${today.getUTCFullYear()}/${today.getUTCMonth()}/${today.getUTCDate()}/
+
+    async createGuildGeneral(guildID) {
+        const guild = this.client.guilds.get(guildID);
+        return {
+            name: guild.name,
+            id: guild.id,
+            //owner: guild.owner.user.username,
+            ownerID: guild.owner_id
+        };
     }
 };
